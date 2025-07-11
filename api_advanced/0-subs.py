f57c1744bbf75047891e603032ff2942d6778885
Michelle-anyika <m.anyika@alustudent.com>
2025-07-11 21:09:51 +0000

    API script

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """
-0-subs.py
+0-subs module
+Queries the Reddit API to get subscriber count for a subreddit
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit.
-    If subreddit is invalid or doesn't exist, return 0."""
+    """
+    Returns the number of subscribers for a given subreddit.
+    Returns 0 if subreddit is invalid or does not exist.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Python3:alu-scripting:v1.0 (by /u/yourusername)'}
-    
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Bot/1.0)'}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        data = response.json()
-        return data['data']['subscribers']
-    except Exception:
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        return 0
+    except requests.RequestException:
         return 0
 
