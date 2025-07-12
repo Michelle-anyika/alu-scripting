04df82d6aae93f31514ba389b0b6d33c3c36a1b8
Michelle-anyika <m.anyika@alustudent.com>
2025-07-12 17:29:46 +0000

    api scripting

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,22 +12,33 @@ def top_ten(subreddit):
     Prints the titles of the first 10 hot posts for a subreddit.
 
     Args:
-        subreddit (str): The subreddit to search.
+        subreddit (str): The subreddit to query.
     """
-    headers = {'User-Agent': 'ALU-Student-Agent/1.0'}
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {
+        'User-Agent': 'My-User-Agent'
+    }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
-            print(None)
+            print("None")
             return
 
-        posts = response.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post.get('data', {}).get('title'))
+        data = response.json().get('data')
+        if not data:
+            print("None")
+            return
 
-    except requests.RequestException:
-        print(None)
+        posts = data.get('children')
+        if not posts:
+            print("None")
+            return
+
+        for post in posts:
+            print(post.get('data').get('title'))
+
+    except Exception:
+        print("None")
 
 
