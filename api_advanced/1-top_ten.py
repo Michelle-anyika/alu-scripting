8726efe39d595cb27cdc3a44a2a7c1b0535284a3
Michelle-anyika <m.anyika@alustudent.com>
2025-07-13 10:48:07 +0000

    api scripting

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-"""Query Reddit API and print titles of top 10 hot posts."""
+"""Query the Reddit API and print titles of top 10 hot posts for a subreddit."""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print titles of first 10 hot posts for a subreddit."""
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+
+    If the subreddit is invalid or an error occurs, prints nothing.
+    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Custom-Agent"}
+    headers = {"User-Agent": "Custom-Agent/1.0"}
     params = {"limit": 10}
 
-    try:
-        res = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
-        if res.status_code != 200:
-            return
-
-        data = res.json().get("data")
-        posts = data.get("children")
-
-        for post in posts:
-            print(post.get("data").get("title"))
-
-    except Exception:
+    if response.status_code != 200:
         return
+
+    data = response.json().get("data")
+    if not data:
+        return
+
+    posts = data.get("children")
+    if not posts:
+        return
+
+    for post in posts:
+        title = post.get("data", {}).get("title")
+        if title:
+            print(title)
