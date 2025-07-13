be47bbf5793d0ebadb46627ff5f6449016715db9
Michelle-anyika <m.anyika@alustudent.com>
2025-07-13 11:32:23 +0000

    api scripting

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,27 +5,21 @@ import requests
 
 def top_ten(subreddit):
     """Print titles of first 10 hot posts or None if invalid."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'linux:alu-api:v1.0 (by /u/Anonymous)'}
-
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'linux:alu-api:v1.0'}
+    params = {'limit': 10}
+    
     try:
-        response = requests.get(
-            url, headers=headers, allow_redirects=False, timeout=5
-        )
+        response = requests.get(url, headers=headers, 
+                              params=params, 
+                              allow_redirects=False,
+                              timeout=5)
         if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
             for post in posts[:10]:
                 print(post.get('data', {}).get('title'))
-            return True
-        return False
-    except (requests.exceptions.RequestException, ValueError):
-        return False
-
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("None")
-    else:
-        if not top_ten(sys.argv[1]):
-            print("None")
+            return
+    except Exception:
+        pass
+    print("None")
