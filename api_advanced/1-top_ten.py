2e1516826475617fd85af1d9e5f5db80414c36a6
Michelle-anyika <m.anyika@alustudent.com>
2025-07-13 11:13:51 +0000

    api scripting

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,18 +4,19 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a subreddit."""
+    """Print titles of first 10 hot posts or None if invalid."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'linux:alu-api-advanced:v1.0 (by /u/Anonymous)'}
+    headers = {'User-Agent': 'linux:alu-api:v1.0 (by /u/Anonymous)'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False, timeout=5)
-    
-    if response.status_code == 200:
-        try:
-            posts = response.json()['data']['children']
+    try:
+        response = requests.get(
+            url, headers=headers, allow_redirects=False, timeout=5
+        )
+        if response.status_code == 200:
+            posts = response.json().get('data', {}).get('children', [])
             for post in posts[:10]:
-                print(post['data']['title'])
-        except (KeyError, ValueError):
+                print(post.get('data', {}).get('title'))
+        else:
             print(None)
-    else:
+    except (requests.exceptions.RequestException, ValueError):
         print(None)
